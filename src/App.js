09b4d1669b9d4b6a685d90beb914d0020149c7d8
Mark Craig <mark.craig65@gmail.com>
2017-10-22 18:43:58 -0700

    Responses button now works, slight improvements to UI

--- src/App.js
+++ src/App.js
@@ -12,49 +12,70 @@ class App extends Component {
       comments: []
     }
     this.update = this.update.bind(this);
+    this.api_url = "https://comments-4orum.herokuapp.com/"
   }
 
   /*Given a url to our api, update state with new data*/
-  update(url) {
-    console.log(this)
-    fetch(url)
-      .then((response) => {
-        return response.json()
-      }).then((json) => {
-          console.log('parsed json', json);
-          this.setState({'comments' : json.comments})
-        }).catch((ex) => {
-          console.log('parsing failed', ex)
-        })
+  update(route) {
+    fetch(this.api_url + route)
+    .then((response) => {
+      return response.json()
+    }).then((json) => {
+      console.log('parsed json', json);
+      json.root_comments
+      ? this.setState({'comments' : json.root_comments})
+      : this.setState({'comments' : [json]})
+    }).catch((ex) => {
+      console.log('parsing failed', ex)
+    })
   }
 
   /*When our component mounts onto the DOM, get data from our server */
   componentDidMount() {
     console.log("component mounted");
-    this.update("https://comments-4orum.herokuapp.com/posts/1/?format=json");
+    this.update("posts/1/?format=json");
   }
+
   render() {
-    if (this.state.comments){
     return (
       <div className="App">
-        <div className="pure-g">
-          <div className="pure-u-1 pure-u-lg-3-4">
-            {this.state.comments.map((comment) =>
-              <Comment
-                author={comment.author}
-                thought={comment.thought}
-                text={comment.text}
-                id={comment.id} />
-              )}
-            </div>
-          </div>
+      <div className="pure-g">
+      <div className="pure-u-1">
+      <CommentThread
+      comments = {this.state.comments}
+      updateRoot = {(route) => this.update(route)}
+      />
       </div>
-    );
-    } else {
-      return(<div className="App"/>);
-    }
+      </div>
+      </div>
+    )
   }
 }
+
+function CommentThread(props) {
+  return (
+    props.comments.map((comment) =>
+        <div className="comment-thread">
+        <Comment
+        author={comment.author}
+        sentiment={comment.sentiment}
+        text={comment.text}
+        id={comment.id}
+        key={comment.id}
+        replies={comment.replies}
+        updateRoot={props.updateRoot}
+        />
+        <div className="replies">
+        <CommentThread
+          comments={comment.replies}
+          updateRoot={props.updateRoot}
+        />
+        </div>
+        </div>
+        )
+      )
+}
+
 
 /*
 Comment has the following fields:
@@ -63,17 +84,24 @@ author, thought, text, id
 class Comment extends Component {
   render() {
     return (
-      <div className="comment" key={this.props.id}>
-        <div className="comment-header">
-          <h3>
-          <strong>{this.props.author}</strong> thinks the post is {this.props.thought}
-          </h3>
-        </div>
-        <div className="comment-body">
-            <p className="comment-text">{this.props.text}</p>
-        </div>
-        <div className="comment-footer">
-        </div>
+      <div className="comment">
+      <div className="comment-header">
+      <h3>
+      <strong>{this.props.author}</strong> thinks this is {this.props.sentiment}
+      </h3>
+      </div>
+      <div className="comment-body">
+      <p className="comment-text">{this.props.text}</p>
+      </div>
+      <div className="comment-footer">
+      <Button
+      label={"Responses"}
+      onClick={() => this.props.updateRoot('comments/'+this.props.id+'/?format=json')}
+      />
+      <Button
+      label={"Reply"}
+      />
+      </div>
       </div>
     );
   }
@@ -85,7 +113,7 @@ Button takes in a label, and a function that corresponds to onClick.
 function Button(props) {
   return (
     <button type="button" className="pure-button" onClick={props.onClick}>
-      {this.props.label}
+    {props.label}
     </button>
   )
 }
